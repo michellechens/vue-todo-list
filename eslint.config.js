58bce69eaf2799ce9cf2567069616fe76dc9fbb5
Michelle Chen <michelle800902@gmail.com>
2024-12-12 15:37:49 +0800

    Add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -7,12 +7,21 @@ export default [
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
+  {
+    rules: {
+      'vue/no-undef-components': 'error',
+      'vue/no-undef-properties': 'error',
+      'vue/eqeqeq': 'warn',
+      'vue/no-unused-properties': 'error',
+      'vue/no-unused-emit-declarations': 'error',
+      'array-callback-return': 'error',
+      eqeqeq: 'warn',
+    },
+  },
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
